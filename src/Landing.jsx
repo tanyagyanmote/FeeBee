943b0d04c88b6c42fd2ef15b41a00fbbc10987f7
Tanya G <tanya@Tanyas-MacBook-Pro.local>
2025-10-24 00:44:46 -0700

    rest of landing page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,9 +4,19 @@ import logo from './Extras/FeeBeeLogo.png'; // adjust if needed
 
 const Landing = () => {
   return (
-    <div id="logo-container">
-      <img src={logo} id="feebeelogo" alt="FeeBee logo" />
-    </div>
+    <section class="hero">
+      <div id="logo-container">
+        <img src={logo} id="feebeelogo" alt="FeeBee logo" />
+        <p id="welcome-text">
+          Know your tariff code before you buy avoid hidden fees at customs!
+        </p>
+      </div>
+      <div class="scroll-down-inline">
+        <span class="arrow">↓</span>
+        <span class="text">Scroll Down</span>
+        <span class="arrow">↓</span>
+      </div>
+    </section>
   );
 };
 
